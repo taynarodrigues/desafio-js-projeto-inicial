053ff21220ac793b4cb72c8a4bb5c624edf4d472
taynarodrigues <taynajesus@gmail.com>
2025-03-01 18:20:15 -0400

    Desafios JS

---
+++ desafio-js-projeto_inicial/app.js
@@ -0,0 +1,20 @@
+
+alert('Boas vindas ao nosso site!');
+
+let nome1 = 'Lua';
+let idade1 = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+
+let nome = prompt('Qual o nome do usuário ?');
+
+let idade = prompt('Digite sua idade');
+if(idade >= 18){
+    alert('Pode tirar a habilitação!.');
+}
